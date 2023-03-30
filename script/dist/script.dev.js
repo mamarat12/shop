92262603b2eb44264032f69a6bc534f1afbfdf58
Marat <mansurov12@gmail.com>
2023-03-30 21:08:34 +0300

    init

---
+++ script/dist/script.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$('.partners-slider-block').slick({
+  // dots: true,/*кружочки прокрутки снизу*/
+  infinite: true,
+
+  /*бесконечная прокрутка*/
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3
+    }
+  }, {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+});
+$(function () {
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 100,
+    values: [0, 85],
+    slide: function slide(event, ui) {
+      $("#amount").val(ui.values[0] + "€ - " + ui.values[1] + " €");
+    }
+  });
+  $("#amount").val(+$("#slider-range").slider("values", 0) + "€ - " + $("#slider-range").slider("values", 1) + " €");
+});
